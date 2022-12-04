a5cb7b2770a53982a36107b42d9efbb9eb62f217
xeno <xeno@lv427.space>
2022-12-04 06:23:13 +0100

    solution to day 4

--- Program.fs
+++ Program.fs
@@ -2,4 +2,25 @@
 
 let lines = File.ReadAllLines "/tmp/aoc/input" |> Array.toList
 
-lines |> List.map (printfn "%A")
+let parse (line:string) =
+    let pair = line.Split [|','|]
+    let pair1 = pair[0].Split [|'-'|]
+    let pair2 = pair[1].Split [|'-'|]
+    ((pair1[0] |> int,pair1[1] |> int),(pair2[0] |> int, pair2[1] |> int))
+
+let parsed = lines |> List.map parse
+
+let expand (((x1,x2),(x3,x4)): (int*int)*(int*int)) =
+    ([x1..x2] |> Set.ofList, [x3..x4] |> Set.ofList)
+let expanded = parsed |> List.map expand
+
+let contained ((s1,s2):Set<int>*Set<int>) =
+    let is = Set.intersect s1 s2
+    is.IsEmpty |> not
+    // is = s1 || is = s2
+
+let a1 = expanded |> List.filter contained |> List.length 
+
+parsed |> List.map (printfn "%A")
+
+printfn $"{a1}"
